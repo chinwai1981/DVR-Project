0bc7dab42e2b72a4dad4c797d8d2e934a70aa384
chinwai1981 <chinwai1981@yahoo.com>
2011-10-11 15:13:37 +0800

    Bug fixed  : added messagebox
    Signed-off-by: chinwai1981 <chinwai1981@yahoo.com>

--- DVR/DVR/Form1.cs
+++ DVR/DVR/Form1.cs
@@ -35,6 +35,7 @@ namespace NetDVR
             if (HIK.Hik_HCNetSDK.NET_DVR_Init())
             {
                 AddMessage("Init Success");
+                MessageBox.Show("Success");
                 m_Devicehandle = new ArrayList();
                 m_DeviceClientInfo = new ArrayList();
                 StartUpDVR();
